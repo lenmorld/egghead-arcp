f938989b638512117b190099e4f02bda1090e4fc
lenmorld <lenmorld@gmail.com>
2019-06-25 20:15:42 -0400

    build toggle component - setState uses function, instead of object - chains a callback to be executed after setstate - shows a clean  (but confusing) destructuring option

--- src/Toggle.jsx
+++ src/Toggle.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import { Switch } from "./Switch";
 
 class Toggle extends React.Component {
+	// class properties
 	state = { on: false };
+	toggle = () =>
+		this.setState(
+			prevState => ({ on: !prevState.on }),
+			() => {
+				this.props.onToggle(this.state.on);
+			}
+		);
+	// setState uses function (instead of object)
+	// and runs callback after
+
+	// prevState => ({ on: !prevState.on })
+	// {on} => ({on: !on})
+
 	render() {
-		return <Switch on={this.state.on} />;
+		return <Switch on={this.state.on} onClick={this.toggle} />;
 	}
 }
 
