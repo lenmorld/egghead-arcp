6ba3fda3c14e15259dde9e91b1a51c62f1e18c44
lenmorld <lenmorld@gmail.com>
2019-08-01 21:16:52 -0400

    compound components - static Toggle.On and Toggle.Off - React.Children.map and React.cloneElement

---
+++ src/lessons/2_Toggle.jsx
@@ -0,0 +1,40 @@
+/* Lesson 2: Basic Toggle Component
+
+- setState function
+- lots of destructuring
+- default function props (default values but a function)
+
+*/
+
+import React from "react";
+import { Switch } from "../Switch";
+
+class Toggle extends React.Component {
+	// class properties
+	state = { on: false };
+	toggle = () =>
+		this.setState(
+			({ on }) => ({ on: !on }),
+			() => {
+				this.props.onToggle(this.state.on);
+			}
+		);
+	// setState uses function (instead of object)
+	// and runs callback after
+
+	render() {
+		return <Switch on={this.state.on} onClick={this.toggle} />;
+	}
+}
+
+// passing a function prop to a stateless component
+// destructuring to only get the onToggle prop
+function Usage({
+	onToggle = (...args) => console.log("onToggle", ...args),
+	name = "Benny"
+}) {
+	console.log(name);
+	return <Toggle onToggle={onToggle} />;
+}
+
+export default Usage;
